00abe1530048b5b25aba33439d21dffa85058b4f
manning1999 <manningcopeland2@gmail.com>
2020-07-11 13:43:20 +1000

    Latest build Fixed scaling issues for various buttons and textViews Added dropdown buttons to the preliminary step tab which shows/hides a textview when pressed Gave the app a proper icon which now shows on phones instead of only the emulator Added an app key which can be used to release the app on the Google Play store Added basic graphics for dropdown buttons and text Moved images from Drawable-v24 to Drawable to allow the app to build properly

--- app/src/main/java/com/weaponreviewsteps/Preliminary.java
+++ app/src/main/java/com/weaponreviewsteps/Preliminary.java
@@ -3,6 +3,7 @@ package com.weaponreviewsteps;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,6 +15,7 @@ import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
@@ -25,6 +27,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
+import static android.view.View.GONE;
 
 public class Preliminary extends Fragment {
 
@@ -64,9 +68,91 @@ public class Preliminary extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        UnderlineKeywords(view.findViewById(R.id.section_labelPrelim));
+        UnderlineKeywords(view.findViewById(R.id.textView2));
+        UnderlineKeywords(view.findViewById(R.id.textView3));
+        UnderlineKeywords(view.findViewById(R.id.textView4));
+
+        ConfigureHiddenTextButton1();
+        ConfigureHiddenTextButton2();
+        ConfigureHiddenTextButton3();
+
+    }
 
 
+
+    boolean isVisible1 = false;
+    private void ConfigureHiddenTextButton1(){
+
+
+
+        final TextView text = (TextView)getActivity().findViewById(R.id.textView2);
+
+        text.setVisibility(GONE);
+
+        Button button = getActivity().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                isVisible1 = !isVisible1;
+
+                if(isVisible1 == true){
+                    text.setVisibility(View.VISIBLE);
+                }else{
+                    text.setVisibility(GONE);
+                }
+            }
+        });
+    }
+
+    boolean isVisible2 = false;
+    private void ConfigureHiddenTextButton2(){
+
+
+
+        final TextView text = (TextView)getActivity().findViewById(R.id.textView3);
+
+        text.setVisibility(GONE);
+
+        Button button = getActivity().findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                isVisible2 = !isVisible2;
+
+                if(isVisible2 == true){
+                    text.setVisibility(View.VISIBLE);
+                }else{
+                    text.setVisibility(GONE);
+                }
+            }
+        });
+    }
+
+    boolean isVisible3 = false;
+    private void ConfigureHiddenTextButton3(){
+
+
+
+        final TextView text = (TextView)getActivity().findViewById(R.id.textView4);
+
+        text.setVisibility(GONE);
+
+        Button button = getActivity().findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                isVisible3 = !isVisible3;
+
+                if(isVisible3 == true){
+                    text.setVisibility(View.VISIBLE);
+                }else{
+                    text.setVisibility(GONE);
+                }
+            }
+        });
     }
 
 
